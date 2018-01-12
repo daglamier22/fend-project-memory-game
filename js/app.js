4d532d20c50d6d8dda4eace50531bc2bba931d76
Brian McCarthy <daglamier22@gmail.com>
2018-01-12 17:17:28 -0600

    removed 0 star rating

--- js/app.js
+++ js/app.js
@@ -187,12 +187,7 @@ function incMoves() {
   //increment
   current += 1;
   //determine star breakpoints
-  if( current > 25 ) {
-    stars.empty();
-    stars.append(openStar);
-    stars.append(openStar);
-    stars.append(openStar);
-  } else if( current > 18 ) {
+  if( current > 18 ) {
     stars.empty();
     stars.append(filledStar);
     stars.append(openStar);
